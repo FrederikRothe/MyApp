b55a5272623b5619c5360706ad33493f1e4f6362
Frederik Rothe <66801011+FrederikRothe@users.noreply.github.com>
2021-09-05 16:10:53 +0200

    Further improvement (#2)
    * Added if-check for years before 1582
    
    * Update on Overleaf.

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,40 +1,39 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace MyApp.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void isDivisbleByFour()
+        public void IsLeapYear_isDivisbleByFour()
         {
-            Assert.True(Program.IsLeapYear(12));
-            Assert.True(Program.IsLeapYear(32));
-
-
+            Assert.False(Program.IsLeapYear(11));
+            Assert.False(Program.IsLeapYear(17));
             Assert.False(Program.IsLeapYear(13));
             Assert.False(Program.IsLeapYear(27));
             Assert.False(Program.IsLeapYear(39));
         }
 
         [Fact]
-        public void isNotDivisbleByHundred()
+        public void IsLeapYear_isNotDivisbleByHundred()
         {
             Assert.False(Program.IsLeapYear(1655));
             Assert.False(Program.IsLeapYear(1267));
             Assert.False(Program.IsLeapYear(1929));
-
             Assert.False(Program.IsLeapYear(1800));
             Assert.False(Program.IsLeapYear(1700));
         }
 
          [Fact]
-        public void isDivisbleByFourHundred()
+        public void IsLeapYear_isDivisbleByFourHundred()
         {
             Assert.True(Program.IsLeapYear(1600));
-            Assert.True(Program.IsLeapYear(1200));
-            Assert.True(Program.IsLeapYear(800));
-            Assert.True(Program.IsLeapYear(400));
+            Assert.True(Program.IsLeapYear(2000));
+            Assert.False(Program.IsLeapYear(1200));
+            Assert.False(Program.IsLeapYear(800));
+            Assert.False(Program.IsLeapYear(400));
         }
     }
 }
